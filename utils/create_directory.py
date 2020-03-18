17203181d4730d1b698063aeabe0a34eb0279d30
joaquin caceres galan <jcg230795@gmail.com>
2020-03-18 20:33:03 +0100

    change os.mkdir to os.makedirs

--- utils/create_directory.py
+++ utils/create_directory.py
@@ -2,7 +2,7 @@ import os
 
 def createDirectory(path_location):
   try:
-    os.mkdir(path_location)
+    os.makedirs(path_location)
   except OSError:  
     print ("Creation of the directory %s failed" % path_location)
   else:  
