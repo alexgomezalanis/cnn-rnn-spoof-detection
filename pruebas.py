921a5098ec35607e0566d1774db6dc143e9b6d44
joaquin caceres galan <jcg230795@gmail.com>
2020-04-07 22:19:04 +0200

    fixing errors

--- pruebas.py
+++ pruebas.py
@@ -1,9 +1,12 @@
 from torch.utils.data import DataLoader #A subpackage that contains utility classes like data sets and data loaders that make data preprocessing easier
 from dataset import CNN_RNN_Dataset
 import os
+import sys
 from utils.collate import collate
 from model import CNN_RNN
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
 
 
@@ -31,12 +34,37 @@ shift=frame_shift, #32
 dataset='training',
 num_classes=num_classes)
 
+model = CNN_RNN(num_classes,n_frames,n_shift,device)
+
 muestra = next(iter(train_dataset))
 
-train_loader = DataLoader(train_dataset, batch_size=3, shuffle=True,
+train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True,
      num_workers=0, collate_fn=collate)
 
 muestras = next(iter(train_loader))
-model = CNN_RNN(num_classes,n_frames,n_shift,device)
-print(muestras)
-model(muestras)
+
+#empezamos el entrenamiento de un solo batch
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),3e-4)
+
+pid = os.getpid()
+epoch=0
+batch_idx=0
+output = model(muestras)
+data = output[0].to(device)
+target = output[1].to(device)
+# loss = criterion(data, target)
+# optimizer.zero_grad() 
+# loss.backward()
+# optimizer.step()
+
+print('len_data',len(data))
+print('train_loader_len',len(train_loader))
+print(train_loader.dataset)
+
+print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+pid, epoch, batch_idx * len(data), len(train_loader.dataset),
+100. * batch_idx / len(train_loader), 66666))
+sys.stdout.flush()
+
