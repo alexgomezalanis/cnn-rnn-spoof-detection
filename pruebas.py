d4a7f09edddf55464a0f3cff28a66bf9107cebe3
joaquin caceres galan <jcg230795@gmail.com>
2020-04-07 23:58:25 +0200

    test

--- pruebas.py
+++ pruebas.py
@@ -38,7 +38,7 @@ model = CNN_RNN(num_classes,n_frames,n_shift,device)
 
 muestra = next(iter(train_dataset))
 
-train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True,
+train_loader = DataLoader(train_dataset, batch_size=3, shuffle=True,
      num_workers=0, collate_fn=collate)
 
 muestras = next(iter(train_loader))
@@ -59,12 +59,17 @@ target = output[1].to(device)
 # loss.backward()
 # optimizer.step()
 
-print('len_data',len(data))
-print('train_loader_len',len(train_loader))
-print(train_loader.dataset)
+print(data)
+print(target)
+print(data.max(1)[1])
+print(data.max(1)[1].eq(target).sum().item())
 
-print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-pid, epoch, batch_idx * len(data), len(train_loader.dataset),
-100. * batch_idx / len(train_loader), 66666))
-sys.stdout.flush()
+# print('len_data',len(data))
+# print('train_loader_len',len(train_loader))
+# print(train_loader.dataset)
+
+# print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+# pid, epoch, batch_idx * len(data), len(train_loader.dataset),
+# 100. * batch_idx / len(train_loader), 66666))
+# sys.stdout.flush()
 
