59edde5a5095f39b3376861e3198398ad9105798
joaquin caceres galan <jcg230795@gmail.com>
2020-04-13 16:57:16 +0200

    fixing errors

--- model.py
+++ model.py
@@ -9,10 +9,10 @@ class CNN_RNN(nn.Module):
     self.device = device
     self.n_frames = n_frames
     self.n_shift = n_shift
-    self.conv1 = nn.Conv2d(1, 16, kernel_size=9, stride=1, padding=2)
-    self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=1, padding=2)
-    self.gru = nn.GRUCell(input_size=32*28*14, hidden_size=32*28*14) #input size = 128*28*14
-    self.fc2 = nn.Linear(32*28*14,num_classes) #input size = 1920 
+    self.conv1 = nn.Conv2d(1, 8, kernel_size=9, stride=1, padding=2)
+    self.conv2 = nn.Conv2d(8, 16, kernel_size=4, stride=1, padding=2)
+    self.gru = nn.GRUCell(input_size=16*28*14, hidden_size=16*28*14)
+    self.fc2 = nn.Linear(16*28*14,num_classes)
   
   def forward(self, x):
     locuciones = x[0]
@@ -24,19 +24,18 @@ class CNN_RNN(nn.Module):
       for ventana in ventanas:
         ventana = ventana.unsqueeze(0) #añadimos una dimensión para decir que el numero de canales es 1
         ventana = ventana.unsqueeze(0) #añadimos otra dimensión para decir que el tamaño del lote es 1
-        y = F.max_pool2d(torch.sigmoid(self.conv1(ventana)), kernel_size=3, stride=3, padding=0)
-        y = F.max_pool2d(torch.sigmoid(self.conv2(y)), kernel_size=3, stride=3, padding=0)
+        y = F.max_pool2d(torch.RReLU(self.conv1(ventana)), kernel_size=3, stride=3, padding=0)
+        y = F.max_pool2d(torch.RReLU(self.conv2(y)), kernel_size=3, stride=3, padding=0)
         y = y.squeeze(0)
         cnn.append(y)
-      y = torch.stack(cnn) #juntamos las diferentes embedding en un solo tensor [nº embeddings,nº canales,lenght,widht]
+      y = torch.stack(cnn)
       y = y.flatten(start_dim=1)
-      hx = torch.randn(1, 32*28*14).to(self.device)
+      hx = torch.randn(1, 16*28*14).to(self.device)
       for i in range(y.shape[0]):
         hx = self.gru(y[i].unsqueeze(0), hx)
       y = self.fc2(hx)
       salida_lineal.append(y)
-    tensor_salida = torch.stack(salida_lineal).squeeze(1)
-    samples = F.softmax(tensor_salida,dim=1)
+    samples = torch.stack(salida_lineal).squeeze(1)
     return (samples, labels)
 
   def calcular_ventanas_espectrales(self,locucion):
@@ -50,10 +49,3 @@ class CNN_RNN(nn.Module):
       start = end - overlap
       end =  start + self.n_frames
     return list_spectral_windows
-
-  def num_flat_features(self, x):
-    size = x.size()[1:]  # all dimensions except the batch dimension
-    num_features = 1
-    for s in size:
-      num_features *= s
-    return num_features
