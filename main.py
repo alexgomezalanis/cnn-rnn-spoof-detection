a4f38b4232f1b1289a579aae639c463f798817be
joaquin caceres galan <jcg230795@gmail.com>
2020-05-01 18:47:52 +0200

    ....

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ parser.add_argument('--load-epoch', type=int, default=-1,
                     help='Saved epoch to load and start training')
 parser.add_argument('--load-trainModel', type=str, default='3',
                     help='path to load train model')
-parser.add_argument('--csv-test', type=str, default='reverAndClipping',
+parser.add_argument('--csv-test', type=str, default='noiseAndRever',
                     help='path to load train model (default: ConjuntoDatosTest)')
 
 
